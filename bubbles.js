7e49012ebaee072a659d967ee2dbdbb74b5677ff
tattoodmac <tattoodmac@gmail.com>
2017-11-13 21:14:26 -0500

    Update bubbles.js

--- bubbles.js
+++ bubbles.js
@@ -4,7 +4,7 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
-var myName = "Welcome to my site!";
+var myName = "WELCOME!";
 var letterColors = [red, orange, green, blue, purple]
 if(10 > 3) {
     bubbleShape = "circle";
